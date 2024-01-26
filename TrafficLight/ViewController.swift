3e0eb9f66f5c2080e612403118087252468f3413
Vladimir <svami.vd@gmail.com>
2024-01-26 12:43:43 +0300

    Add logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = redLightView.bounds.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.bounds.width / 2
+        greenLightView.layer.cornerRadius = yellowLightView.bounds.width / 2
+        // почему на разных устройствах скругление выглядит по разному?
+        
+        startButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } else if yellowLightView.alpha == 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+        } else {
+            redLightView.alpha = 1
+        }
+    }
+    
 }
 
